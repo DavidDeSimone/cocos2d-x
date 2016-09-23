0ffb47aacf37e5bbcc2c7d28d1db4386bfdc731b
DavidDeSimone <djd231@scarletmail.rutgers.edu>
2016-09-22 20:03:30 -0700

    Adding 'addImageAsyncImmediate' to CCTextureCache. This will allow engine users to load multiple textures async across multiple threads, or load a texture before other textures in the texture loading queue.

--- cocos/renderer/CCTextureCache.h
+++ cocos/renderer/CCTextureCache.h
@@ -136,6 +136,17 @@ public:
      */
     virtual void unbindAllImageAsync();
 
+
+    /** Returns a Texture2D object given a file image. This function will create a new worker thread and immediatly begin loading the image
+      * If the file image was not previously loaded, it will create a new Texture2D object and it will return it.
+      * Otherwise it will load a texture in a new thread, and when the image is loaded, the callback will be called with the Texture2D as a parameter.
+      * The callback will be called from the main thread, so it is safe to create any cocos2d object from the callback.
+      * Supported image extensions: .png, .jpg
+     @param filepath A null terminated string.
+     @param callback A callback function would be invoked after the image is loaded.
+      */
+    virtual void addImageAsyncImmediate(const std::string &filename, const std::function<void(Texture2D*)>& callback);
+
     /** Returns a Texture2D object given an Image.
     * If the image was not previously loaded, it will create a new Texture2D object and it will return it.
     * Otherwise it will return a reference of a previously loaded image.
@@ -227,6 +238,7 @@ protected:
     std::thread* _loadingThread;
 
     std::deque<AsyncStruct*> _asyncStructQueue;
+    std::deque<AsyncStruct*> _immediateAsyncStructQueue;
     std::deque<AsyncStruct*> _requestQueue;
     std::deque<AsyncStruct*> _responseQueue;
 
